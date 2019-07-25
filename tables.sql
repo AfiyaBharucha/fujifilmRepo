04392c19c07ce6b4112202c32ed9dcb9c30b60f1
AfiyaBharucha <51459840+AfiyaBharucha@users.noreply.github.com>
2019-07-25 12:57:37 -0400

    added tables from MySQL.

--- tables.sql
+++ tables.sql
@@ -13,3 +13,27 @@ contact varchar(255) not null, username varchar(255) not null unique, password v
 
 create table product_master
 (product_id int auto_increment primary key,product_name varchar(255) unique,price varchar(25),Qty varchar(25));
+
+
+create table order(
+Order_No int(11) 
+id int(11) 
+product_id int(11) 
+Qty_Sold int(11) 
+Order_Date varchar(45) 
+Order_Status varchar(45));
+
+create table inquiry_data(
+Inquiry_Id int(11) 
+product_id int(11) 
+id int(11) 
+Qty int(11) 
+date varchar(45) 
+Status varchar(45));
+
+create table payment(
+Order_No int(10) 
+Pay_Due int(11) 
+Pay_Received int(11) 
+Date varchar(45) 
+id int(11));
